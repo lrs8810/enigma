f49093728774292f4ab2ea9a9c0e7c474bb83c1a
Laura Schulz <lrs8810@gmail.com>
2019-09-16 22:42:57 -0600

    Add tests for encrypt method and initialization

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,17 +7,24 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_exists
-    skip
-    assert_instance_of Enigma, @engima
+    assert_instance_of Enigma, @enigma
   end
 
-  def test_it_can_encrypt_a_message
-    skip
+  def test_it_can_encrypt_a_message_with_a_key_and_message
     expected = {
                 encryption: "keder ohulw",
                 key: "02715",
                 date: "040895"
                 }
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_encrypt_a_message_with_a_key_and_today_date
+    expected = {
+                encryption: "njhauesdxq ",
+                key: "02715",
+                date: "160919"
+                }
+    assert_equal expected, @enigma.encrypt("hello world", "02715")
   end
 end
