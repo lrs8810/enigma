4c02795ba637f73d41f99d232a5b4abc7b8c6c4c
Laura Schulz <lrs8810@gmail.com>
2019-09-17 17:03:53 -0600

    Add tests for decrypt cipher and helper methods

--- test/decryptor_test.rb
+++ test/decryptor_test.rb
@@ -10,7 +10,22 @@ class DecryptorTest < Minitest::Test
     assert_instance_of Decryptor, @decryptor
   end
 
-  def test_decrypt_message
+  def test_it_can_calculate_based_on_character_index
+    expected = [3, 0, 19, 20]
+    assert_equal expected, @decryptor.calculate_shift_based_on_character_index("keder ohulw", "02715", "040895")
+  end
+
+  def test_find_cipher_index
+    expected = [8, 5, -15, -15, 15, 27, -4, -12, 18, 12, 4]
+    assert_equal expected, @decryptor.find_index("keder ohulw", "02715", "040895")
+  end
+
+  def test_find_final_cipher_index
+    expected = [8, 5, 12, 12, 15, 27, 23, 15, 18, 12, 4]
+    assert_equal expected, @decryptor.find_final_index("keder ohulw", "02715", "040895")
+  end
+
+  def test_decrypt_cipher
     assert_equal "hello world", @decryptor.decrypt_cipher("keder ohulw", "02715", "040895")
   end
 end
