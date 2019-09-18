35c2979a32476eef883cee55a6fc2ca4533171b2
Laura Schulz <lrs8810@gmail.com>
2019-09-18 07:31:47 -0600

    Refactor build encryption method and helper methods on Encryptor class

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -4,53 +4,50 @@ require 'date'
 
 class Encryptor
   include Shiftable
-  attr_reader :random_key
+  attr_reader :random_key, :current_date
 
   def initialize
     @random_key = RandomNumGenerator.generate_random_num
+    @current_date = (DateTime.now.strftime "%d%m%y").to_i
   end
 
-  def find_index(message, key, date)
+  def find_encrypt_index(message, key, date)
     dc_message = message.downcase.chars
-    shift_arr = calculate_shift_based_on_character_index(message, key, date)
-    final = []
+    shifts_key = calculate_shift_based_on_character_index(message, key, date)
+    indexes = []
       dc_message.each_with_index do |letter, index|
-        final << letter if !character_set.include?(letter)
-        final << (shift_arr[0] + character_index[letter]) if character_set.include?(letter) && index % 4 == 0
-        final << (shift_arr[1] + character_index[letter]) if character_set.include?(letter) && index % 4 == 1
-        final << (shift_arr[2] + character_index[letter]) if character_set.include?(letter) && index % 4 == 2
-        final << (shift_arr[3] + character_index[letter]) if character_set.include?(letter) && index % 4 == 3
+        indexes << letter if !character_set.include?(letter)
+        indexes << (shifts_key[0] + character_index[letter]) if character_set.include?(letter) && index % 4 == 0
+        indexes << (shifts_key[1] + character_index[letter]) if character_set.include?(letter) && index % 4 == 1
+        indexes << (shifts_key[2] + character_index[letter]) if character_set.include?(letter) && index % 4 == 2
+        indexes << (shifts_key[3] + character_index[letter]) if character_set.include?(letter) && index % 4 == 3
       end
-    final
+    indexes
   end
 
-  def find_final_index(message, key, date)
-    final = find_index(message, key, date)
-    final_arr = []
-    final.each do |shift|
-      if shift.class == String
-        final_arr << shift
-      elsif shift > 27
-        final_arr << shift - character_index.length
+  def find_final_encrypt_index(message, key, date)
+    indexes = find_encrypt_index(message, key, date)
+    indexes.inject([]) do |final_indexes, shift|
+      if shift.class == Integer && shift > 27
+        final_indexes << shift - character_index.length
       else
-        final_arr << shift
+        final_indexes << shift
       end
     end
-    final_arr
   end
 
   def build_encryption(message, key, date)
-    final_shifts = find_final_index(message, key, date)
+    final_shifts = find_final_encrypt_index(message, key, date)
     final_shifts.map do |letter|
       if letter.class == String
-        letter
-      else
-        character_index.key(letter)
+          letter
+        else
+          character_index.key(letter)
       end
     end.join
   end
 
   def encrypt_message(message, key = nil, date = nil)
-    build_encryption(message, key || @random_key, date || current_date)
+    build_encryption(message, key || @random_key, date || @current_date)
   end
 end
