767157bd8051243ed5548b267042ff070aad47b2
Laura Schulz <lrs8810@gmail.com>
2019-09-18 02:53:36 -0600

    Fix naming convention on decryptor runner file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -6,12 +6,12 @@ cipher = File.open(ARGV[0], "r")
 
 incoming_cipher = cipher.read
 
-decrypt_message = @enigma.decrypt(incoming_cipher, ARGV[2], ARGV[3] || date = nil)
+decrypted_message = @enigma.decrypt(incoming_cipher, ARGV[2], ARGV[3] || date = nil)
 
 message = File.open(ARGV[1], "w")
 
-message.write(decrypt_message[:decryption])
+message.write(decrypted_message[:decryption])
 
 message.close
 
-p "Created 'decrypt.txt' with the key #{decrypt_message[:key]} and date #{decrypt_message[:date]}"
+p "Created 'decrypt.txt' with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
